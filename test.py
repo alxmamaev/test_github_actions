e5220eb219a2b6f693387cc3656b6c63f1fe0ed6
Alexander Mamaev <alxmamaev@ya.ru>
2022-02-14 19:50:18 +0300

    Add telegram bot

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import json
+import os
 from pathlib import Path
-
+import telebot
 
 
 def main():
+    telegram_bot_api_key = os.environ["TELEGRAM_BOT_KEY"]
+    target_person_id = int(os.environ["TARGET_ID"])
+
+    bot = telebot.TeleBot(telegram_bot_api_key, parse_mode=None)
+
     for p in Path("./").glob("*.json"):
         with p.open("r") as f:
             data = json.load(f)
@@ -12,6 +18,7 @@ def main():
             assert data["key"] == "value", "wrong value"
 
     print("All tests passed")
+    bot.send_message(target_person_id, "All tests passed")
 
 if __name__ == "__main__":
     main()
